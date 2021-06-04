ca8d3457482e4cc9736eb5e89ebe6db2040c6c93
Dmitriy <aziraal007@yandex.ru>
2021-06-04 16:39:20 +0300

    dispatch serializer.py to GitHub

---
+++ authentication/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    """ Сериализация регистрации пользователя и создания нового. """
+
+    password = serializers.CharField(max_length=128, min_length=8, write_only=True)
+
+    # Клиентская сторона не должна иметь возможность отправлять токен вместе с
+    # запросом на регистрацию. Сделаем его доступным только на чтение.
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        # Перечислить все поля, которые могут быть включены в запрос
+        # или ответ, включая поля, явно указанные выше.
+        fields = ['email', 'username', 'password', 'token']
+
+    def create(self, validated_data):
+        # Использовать метод create_user, который мы
+        # написали ранее, для создания нового пользователя.
+        return User.objects.create_user(**validated_data)
